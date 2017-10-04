dcaf89cc16f60e6e621a71d58804661194ca4c20
victor-PC\victor <victor.m.pelaez@outlook.com>
2017-10-04 21:38:36 +0200

    [UPDATE] rename __init__.py and setup requieres

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ import os
 # python setup.py bdist_wheel --universal
 # cd C:\Users\victor\Anaconda3\Scripts
 # twine upload C:/Users/victor/Documents/Repositorios/coral-reef-optimization-algorithm/dist/*
+# pip install cro --upgrade --no-cache-dir
 
 with open('README.txt') as file:
     long_description = file.read()
 
 setup(
     name='cro',
-    version='0.0.2',
+    version='0.0.3',
     author='Victor Pelaez',
     author_email='victor.m.pelaez@outlook.com',
     packages= ['cro'],
@@ -24,6 +25,10 @@ setup(
     long_description= long_description,
     keywords ='optimization algorithm meta-heuristic coral reef',
     python_requires='>=3',
+	install_requires=[
+        'numpy>=1.13.1',
+        'scikit-learn>=0.18.0'
+		],
     classifiers = [],
 
 )
