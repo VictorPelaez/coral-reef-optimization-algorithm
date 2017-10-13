0616538caf3dbfd1683ef8e76d9a8d7eee73142a
apastors <a.pastor.sanchez@gmail.com>
2017-10-13 19:43:33 +0200

    added fitness_coral definition in tests

--- cro/tests.py
+++ cro/tests.py
@@ -28,6 +28,7 @@ class TestCRO(unittest.TestCase):
         L= 8
         N=2
         M=2
+        fitness_coral = lambda coral: 1 # Dummy fitness
         cro = CRO(Ngen=10, N=N, M=M, Fb=0.7, Fa=.1, Fd=.1, r0=.6, k=3, Pd=.1,
                   fitness_coral=fitness_coral, opt='max', L=L)
         (REEF, REEFpob) = cro.reefinitialization()
