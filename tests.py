9fa6c5662323bb3dd9c4b3e9375ef0dd3573eada
Victor Peláez <victor.m.pelaez@outlook.com>
2017-10-13 12:31:53 +0200

    Update and rename test.py to tests.py

--- tests.py
+++ tests.py
@@ -4,13 +4,21 @@
 
 import os
 import sys
+import unittest
+import awesome
 
 try:
     import numpy as np
 except ImportError:
     import numpy
-    
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(awesome.smile(), ":)")
+
 ###############################################################################
 
 if __name__ == "__main__":
+ 
+    unittest.main()
     sys.exit(0)
+
