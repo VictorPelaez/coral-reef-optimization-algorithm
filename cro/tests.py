cb1a53c3d6229714ce33a4127a9bdcd67139803a
victor-PC\victor <victor.m.pelaez@outlook.com>
2017-10-13 16:28:56 +0200

    [UPDATE] tests.py with croInit

--- cro/tests.py
+++ cro/tests.py
@@ -12,23 +12,6 @@ try:
 except ImportError:
     import numpy 
 
-    
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)    
-
 class TestCRO(unittest.TestCase):
 
     def test_croCreation(self):
@@ -38,6 +21,15 @@ class TestCRO(unittest.TestCase):
         cro = CRO(Ngen=10, N=N, M=M, Fb=0.7, Fa=.1, Fd=.1, r0=.6, k=3, Pd=.1, opt='max', L=L)
         (REEF, REEFpob) = cro.reefinitialization()
         self.assertEqual(REEFpob.shape, (N*M, L))
+        
+    def test_croInit(self):
+        L= 8
+        N=2
+        M=2
+        cro = CRO(Ngen=10, N=N, M=M, Fb=0.7, Fa=.1, Fd=.1, r0=.6, k=3, Pd=.1, opt='max', L=L)
+        (REEF, REEFpob) = cro.reefinitialization()
+        self.assertGreaterEqual(len(np.where(REEF!=0)[0]), 0)
+        
             
             
 ###############################################################################
