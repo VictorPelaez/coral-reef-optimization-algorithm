92b9c4b0a5e4699ecf9405982f931c8a964933e6
Victor Peláez <victor.m.pelaez@outlook.com>
2017-10-13 12:33:56 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -11,9 +11,23 @@ try:
     import numpy as np
 except ImportError:
     import numpy
-class TestMethods(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(awesome.smile(), ":)")
+
+    
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)    
 
 ###############################################################################
 
