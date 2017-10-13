a8e9c6dae8a4f657f050d3b7abd63d6949e316ba
Victor Peláez <victor.m.pelaez@outlook.com>
2017-10-13 11:37:28 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+###############################################################################
+
+import os
+import sys
+
+try:
+    import numpy as np
+except ImportError:
+    import numpy
+    
+###############################################################################
+
+if __name__ == "__main__":
+    sys.exit(0)
