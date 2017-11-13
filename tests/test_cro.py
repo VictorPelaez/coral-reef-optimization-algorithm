fe4dff37cf573612e7898b664917cbd5e8d5fbb8
apastors <a.pastor.sanchez@gmail.com>
2017-11-13 19:55:18 +0100

    move insert path to __init_-

--- tests/test_cro.py
+++ tests/test_cro.py
@@ -3,11 +3,6 @@
 ###############################################################################
 # run with:
 # python -m pytest tests/test_cro.py
-
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import numpy as np
 
 from cro.cro import CRO
--- tests/test_reefinitialization.py
+++ tests/test_reefinitialization.py
@@ -3,28 +3,28 @@
 ###############################################################################
 # run with:
 # python -m pytest tests/test_larvaemutation.py
-
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import numpy as np
 
-#from cro.reefinitialization import bin_binary, disc_equal_range
+from cro.reef_initialization import bin_binary, disc_equal_range
 
 # ------------------------------------------------------
 # reefinitialization module
 # ------------------------------------------------------
-
 def test_bin_binary():
     """
     Test that corals in the population only contain values in {0, 1}
     """
-    pass
-    
-    
+    M, N, r0, L = 2, 2, 0.6, 8
+    REEF, REEFpob = bin_binary(M, N, r0, L)
+    assert set(REEFpob.ravel()) == {0, 1}
+
 def test_disc_equal_range():
     """
     Test that corals in population contain values specified in the grid
     """
-    pass
+    M, N, r0, L = 2, 2, 0.6, 8
+    grid = {'x': [2, 10]}      # Discrete values between 2 and 10
+
+    REEF, REEFpob = disc_equal_range(M, N, r0, L, param_grid=grid)
+    p = sum(REEFpob[np.where(REEFpob!=0)]<grid['x'][0]) + sum(REEFpob[np.where(REEFpob!=0)]>grid['x'][1])
+    assert p == 0
